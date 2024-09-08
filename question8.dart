78c8514cc5552a97be06c5ee3cbe95c43899a40c
muzammilhadiqa901@gmail.com <abdullahbinaslam64@gmail.com>
2024-09-08 11:17:50 +0500

    2 commit

--- question8.dart
+++ question8.dart
@@ -1,8 +1,9 @@
 // Q.8: remove all false values from below list by using removeWhere or retainWhere property.
 // List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
 void main(){
-  List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike'];
-  usersEligibility.removeWhere((element) => element.toLowerCase() != 'eligible');
+  List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+  usersEligibility.removeWhere((element) => element == 'eligible');
+  //usersEligibility.retainWhere((element) => element != 'eligible');
   print(usersEligibility);
 
 
